2fbc602eebc85f02f1db256e98b1b492a8205068
Emilio Islas <exinyo@gmail.com>
2017-02-21 23:32:35 -0800

    Edits
    Map changes
    Plugin list changes

--- js/plugins.js
+++ js/plugins.js
@@ -5,7 +5,7 @@ var $plugins =
 {"name":"QPlus","status":true,"description":"<QPlus> (Should go above all Q Plugins)\r\nSome small changes to MV for easier plugin development.","parameters":{"Quick Test":"true","Default Enabled Switches":"1"}},
 {"name":"QUpdate","status":true,"description":"<QUpdate>\r\nChecks QPlugins for updates","parameters":{}},
 {"name":"========================","status":true,"description":"","parameters":{}},
-{"name":"QInput","status":true,"description":"<QInput>\r\nAdds additional keys to Input class, and allows remapping keys.","parameters":{"Ok":"#enter, #space, #z","Escape / Cancel":"#esc, #insert, #x, #num0","Shift":"#shift, #cancel","Control":"#ctrl, #alt","Tab":"#tab","Pageup":"#pageup, #q","Pagedown":"#pagedown, #w","Left":"#left, #num4, #a","Right":"#right, #num6, #d","Up":"#up, #num8, #w","Down":"#down, #num2, #s","Debug":"#f9","===========":"","FPS":"f2","Streched":"f3","FullScreen":"f4","Restart":"f5","Console":"f8"}},
+{"name":"QInput","status":true,"description":"<QInput>\r\nAdds additional keys to Input class, and allows remapping keys.","parameters":{"Ok":"#enter, #space, #z, $A","Escape / Cancel":"#esc, #insert, #x, #num0, $B","Menu":"$Y","Shift":"#shift, #cancel, $X","Control":"#ctrl, #alt","Tab":"#tab","Pageup":"#pageup, #q, $L1","Pagedown":"#pagedown, #w, $R1","Left":"#left, #num4, $LEFT, #a","Right":"#right, #num6, $RIGHT, #d","Up":"#up, #num8, $UP, #w","Down":"#down, #num2, $DOWN, #s","Debug":"#f9","===========":"","FPS":"f2","Streched":"f3","FullScreen":"f4","Restart":"f5","Console":"f8"}},
 {"name":"QInputRemap","status":true,"description":"<QInputRemap>\r\nQuasi Input Addon: Adds Key remapping to Options menu","parameters":{"Hide Keys":"","Disable Keys":"ok, escape","===========":"","Vocab: Ok":"Action","Vocab: Escape":"Cancel","Vocab: Shift":"Run","Vocab: Control":"Control","Vocab: Tab":"Tab","Vocab: Pageup":"Next","Vocab: Pagedown":"Prev","Vocab: Up":"Up","Vocab: Down":"Down","Vocab: Left":"Left","Vocab: Right":"Right"}},
 {"name":"QNameInput","status":true,"description":"<QNameInput>\r\nQuasi Input addon: Adds Keyboard Input to Name Input Scene","parameters":{"Show Window with Keys":"false","Window Width":"480"}},
 {"name":"QTouch","status":true,"description":"<QTouch>\nBetter mouse handling for MV","parameters":{"Mouse Decay":"60","Default Cursor":"","Pointer Cursor":""}},
@@ -14,5 +14,6 @@ var $plugins =
 {"name":"QSprite","status":true,"description":"<QSprite>\r\nLets you configure Spritesheets","parameters":{"File Name Identifier":"%{config}-","Random Idle Interval":"60 300","Use New Adjust":"true"}},
 {"name":"QAudio","status":true,"description":"<QAudio>\r\nFew new audio features","parameters":{"Default Radius":"5","Default Max Volume":"100"}},
 {"name":"QSpeed","status":true,"description":"<QSpeed>\r\nAllows for custom Move speeds and an acceleration effect","parameters":{"Acceleration":"true","Duration":"30"}},
-{"name":"QCamera","status":true,"description":"<QCamera>\r\nBetter Camera control","parameters":{"Offset":"0.5"}}
+{"name":"QCamera","status":true,"description":"<QCamera>\r\nBetter Camera control","parameters":{"Offset":"0.5"}},
+{"name":"QPathfind","status":true,"description":"<QPathfind>\r\nA* Pathfinding algorithm","parameters":{"Diagonals":"false","Intervals":"100","Smart Wait":"60","=============":"","Half Opt":"true"}}
 ];
