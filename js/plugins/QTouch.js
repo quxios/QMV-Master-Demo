32bdb48bf4f4b3ca968c0cad403d83f8acad811f
Emilio Islas <exinyo@gmail.com>
2017-08-26 01:51:19 -0700

    QTouch 1.1.1
    Fix for arrows not being clickable
    Removed QButton class (will be it's own plugin)

--- js/plugins/QTouch.js
+++ js/plugins/QTouch.js
@@ -3,7 +3,7 @@
 //=============================================================================
 
 var Imported = Imported || {};
-Imported.QTouch = '1.1.0';
+Imported.QTouch = '1.1.1';
 
 if (!Imported.QPlus || !QPlus.versionCheck(Imported.QPlus, '1.4.0')) {
   alert('Error: QTouch requires QPlus 1.4.0 or newer to work.');
@@ -14,7 +14,7 @@ if (!Imported.QPlus || !QPlus.versionCheck(Imported.QPlus, '1.4.0')) {
  /*:
  * @plugindesc <QTouch>
  * Better mouse handling for MV
- * @author Quxios  | Version 1.1.0
+ * @author Quxios  | Version 1.1.1
  *
  * @video https://youtu.be/2UrazG-XRxw
  *
@@ -262,12 +262,24 @@ function Sprite_QButton() {
           this._isPointing = true;
           this.select(hitIndex);
         } else if (hitIndex < 0) {
-          this._isPointing = false;
+          if (y < this.padding) {
+            this._isPointing = true;
+          } else if (y >= this.height - this.padding) {
+            this._isPointing = true;
+          } else {
+            this._isPointing = false;
+          }
         }
         if (TouchInput.isTriggered()) {
           if (hitIndex >= 0) {
             if (hitIndex === this.index() && this.isTouchOkEnabled()) {
               this.processOk();
+            }
+          } else {
+            if (y < this.padding) {
+              this.cursorUp();
+            } else if (y >= this.height - this.padding) {
+              this.cursorDown();
             }
           }
         }
@@ -294,125 +306,5 @@ function Sprite_QButton() {
 
   Sprite_Button.prototype.isPointing = function() {
     return this.isActive() && this.isButtonTouched();
-  };
-
-  //-----------------------------------------------------------------------------
-  // Sprite_QButton
-
-  Sprite_QButton.prototype = Object.create(Sprite_Button.prototype);
-  Sprite_QButton.prototype.constructor = Sprite_QButton;
-
-  Sprite_QButton.prototype.initialize = function() {
-    Sprite_Button.prototype.initialize.call(this);
-    this._overTick = 0;
-    this._doubleClickTimer = 0;
-    this._doubleClickHandler = null;
-    this._rightClickHandler = null;
-    this._mouseEnterHandler = null;
-    this._mouseExitHandler = null;
-  };
-
-  Sprite_QButton.prototype.setDoubleClick = function(method) {
-    this._doubleClickHandler = method;
-  };
-
-  Sprite_QButton.prototype.setRightClick = function(method) {
-    this._rightClickHandler = method;
-  };
-
-  Sprite_QButton.prototype.setMouseEnter = function(method) {
-    this._mouseEnterHandler = method;
-  };
-
-  Sprite_QButton.prototype.setMouseExit = function(method) {
-    this._mouseExitHandler = method;
-  };
-
-  Sprite_QButton.prototype.setMouseOver = function(method) {
-    this._mouseOverHandler = method;
-  };
-
-  Sprite_QButton.prototype.setMouseOut = function(method) {
-    this._mouseOutHandler = method;
-  };
-
-  Sprite_QButton.prototype.callDoubleClickHandler = function() {
-    if (this._doubleClickHandler) {
-      this._doubleClickHandler();
-    }
-  };
-
-  Sprite_QButton.prototype.callRightClickHandler = function() {
-    if (this._rightClickHandler) {
-      this._rightClickHandler();
-    }
-  };
-
-  Sprite_QButton.prototype.callMouseEnterHandler = function() {
-    if (this._mouseEnterHandler) {
-      this._mouseEnterHandler();
-    }
-  };
-
-  Sprite_QButton.prototype.callMouseExitHandler = function() {
-    if (this._mouseExitHandler) {
-      this._mouseExitHandler();
-    }
-  };
-
-  Sprite_QButton.prototype.callMouseOverHandler = function() {
-    if (this._mouseOverHandler) {
-      this._mouseOverHandler();
-    }
-  };
-
-  Sprite_QButton.prototype.callMouseOutHandler = function() {
-    if (this._mouseOutHandler) {
-      this._mouseOutHandler();
-    }
-  };
-
-  Sprite_QButton.prototype.update = function() {
-    if (this._doubleClickTimer > 0) {
-      this._doubleClickTimer--;
-    }
-    Sprite_Button.prototype.update.call(this);
-  };
-
-  Sprite_QButton.prototype.processTouch = function() {
-    if (this.isActive()) {
-      if (this.isButtonTouched()) {
-        if (this._overTick === 0) {
-          this.callMouseEnterHandler();
-        } else {
-          this.callMouseOverHandler();
-        }
-        this._overTick++;
-      } else {
-        if (this._overTick !== 0) {
-          this.callMouseExitHandler();
-        } else {
-          this.callMouseOutHandler();
-        }
-        this._overTick = 0;
-      }
-      if (TouchInput.isTriggered() && this.isButtonTouched()) {
-        this._touching = true;
-      }
-      if (TouchInput.isTriggered()) {
-        if (this._doubleClickTimer > 0) {
-          this._doubleClickTimer = 0;
-          this.callDoubleClickHandler();
-        } else {
-          this._doubleClickTimer = 30;
-          this.callClickHandler();
-        }
-      }
-      if (TouchInput.isCancelled() && this.isButtonTouched()) {
-        this.callRightClickHandler();
-      }
-    } else {
-      this._touching = false;
-    }
   };
 }());
